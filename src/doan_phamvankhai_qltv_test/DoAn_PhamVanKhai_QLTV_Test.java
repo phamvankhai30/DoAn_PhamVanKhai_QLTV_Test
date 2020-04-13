55cdf68b7c10ef77824356e474ffce9be8ad8f20
khai <khai@192.168.78.1>
2020-04-13 08:47:01 +0700

    This is DoAn

---
+++ src/doan_phamvankhai_qltv_test/DoAn_PhamVanKhai_QLTV_Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doan_phamvankhai_qltv_test;
+
+/**
+ *
+ * @author khai
+ */
+public class DoAn_PhamVanKhai_QLTV_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
